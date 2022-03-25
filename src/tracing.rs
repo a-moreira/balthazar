b754c37f4f4792f5e2292dea732e7abb25d9de09
drake-cloudwalk <drake@cloudwalk.io>
2022-03-25 11:44:21 -0300

    feat: allow disable telemetry

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,4 +1,4 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, Registry, util::SubscriberInitExt};
 use tracing_tree::HierarchicalLayer;
 
 use crate::{EnvironmentConfig, Feature, Parser, Result};
@@ -13,6 +13,9 @@ pub struct TracingConfig {
 
     #[clap(env = "TRACING_LOG_LEVEL", default_value = "debug")]
     pub log_level: String,
+
+    #[clap(long, env = "NO_TELEMETRY")]
+    pub no_telemetry: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -22,23 +25,27 @@ impl Feature for Tracing {
     fn init(config: EnvironmentConfig) -> Result<Self> {
         std::env::set_var("RUST_LOG", &config.tracing.log_level);
 
-        let tracer = opentelemetry_jaeger::new_pipeline()
-            .with_collector_endpoint(&config.tracing.opentelemetry_endpoint)
-            .with_service_name(module_path!())
-            .install_batch(opentelemetry::runtime::Tokio)?;
+        let should_init_telemetry = !config.tracing.no_telemetry;
 
-        let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+        if (should_init_telemetry) {
+            let tracer = opentelemetry_jaeger::new_pipeline()
+                .with_collector_endpoint(&config.tracing.opentelemetry_endpoint)
+                .with_service_name(module_path!())
+                .install_batch(opentelemetry::runtime::Tokio)?;
 
-        Registry::default()
-            .with(EnvFilter::from_default_env())
-            .with(
-                HierarchicalLayer::new(2)
-                    .with_targets(true)
-                    .with_bracketed_fields(true)
-                    .with_ansi(config.core.no_color),
-            )
-            .with(telemetry)
-            .init();
+            let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+            Registry::default()
+                .with(EnvFilter::from_default_env())
+                .with(
+                    HierarchicalLayer::new(2)
+                        .with_targets(true)
+                        .with_bracketed_fields(true)
+                        .with_ansi(config.core.no_color),
+                )
+                .with(telemetry)
+                .init();
+        }
 
         tracing::debug!("started tracer");
 
